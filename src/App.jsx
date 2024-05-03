562ea060a23858c845783f376adbe2128872d3f7
MichelaVivacqua <vivacquamichela@gmail.com>
2024-05-03 11:44:10 +0200

    registrazione page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import MyNavbar from "./components/MyNavbar";
 import Login from "./components/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Registrazione from "./components/Registrazione";
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
         <main className="container-fluid">
           <Routes>
             <Route element={<Login />} path="/" />
-            {/* <Route element={<Registrazione/>} path="/Registrazione" /> */}
+            <Route element={<Registrazione />} path="/Registrazione" />
             <Route
               element={
                 <h1 className="text-center">ERRORE 404 - PAGINA NON TROVATA</h1>
---
+++ src/components/Registrazione.jsx
@@ -0,0 +1,38 @@
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+
+const Registrazione = () => (
+  <Form>
+    <div className="m-2">Inserisci qui i tuoi dati per registrarti!</div>
+    <Form.Group className="m-2">
+      <Form.Control type="text" placeholder="Username" />
+    </Form.Group>
+
+    <Form.Group className="m-2">
+      <Form.Control type="text" placeholder="Nome" />
+    </Form.Group>
+
+    <Form.Group className="m-2">
+      <Form.Control type="text" placeholder="Cognome" />
+    </Form.Group>
+
+    <Form.Group className="m-2" controlId="formBasicEmail">
+      <Form.Control type="email" placeholder="Email" />
+    </Form.Group>
+
+    <Form.Group className="m-2" controlId="formBasicPassword">
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+
+    <Form.Group className="m-2">
+      <Form.Control type="text" placeholder="Immagine del profilo" />
+    </Form.Group>
+
+    <Button type="submit" className="custom-button">
+      Crea il mio account
+    </Button>
+  </Form>
+);
+
+export default Registrazione;
