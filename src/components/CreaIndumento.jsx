754500c5463084b2c767a0c7e8701c3332c0840b
MichelaVivacqua <vivacquamichela@gmail.com>
2024-05-09 15:57:30 +0200

    selezione tipo e colore da dropdown

--- src/components/CreaIndumento.jsx
+++ src/components/CreaIndumento.jsx
@@ -115,7 +115,7 @@ const CreaIndumento = () => {
           />
         </Form.Group>
 
-        <Form.Group className="m-3">
+        {/* <Form.Group className="m-3">
           <Form.Control
             type="text"
             placeholder="Colore"
@@ -131,6 +131,65 @@ const CreaIndumento = () => {
             value={tipo}
             onChange={(e) => setTipo(e.target.value)}
           />
+        </Form.Group> */}
+
+        <Form.Group className="m-3">
+          <Form.Select
+            value={colore}
+            onChange={(e) => setColore(e.target.value)}
+          >
+            <option value="">Colore</option>
+            <option value="bianco">Bianco</option>
+            <option value="NERO">Nero</option>
+            <option value="bordeaux">Bordeaux</option>
+            <option value="rosso">Rosso</option>
+            <option value="arancione">Arancione</option>
+            <option value="giallo">Giallo</option>
+            <option value="verde">Verde</option>
+            <option value="blu">Blu</option>
+            <option value="ciano">Ciano</option>
+            <option value="azzurro">Azzurro</option>
+            <option value="viola">Viola</option>
+            <option value="lilla">Lilla</option>
+            <option value="rosa">Rosa</option>
+            <option value="magenta">Magenta</option>
+            <option value="grigio">Grigio</option>
+            <option value="marrone">Marrone</option>
+            <option value="beige">Beige</option>
+            <option value="oro">Oro</option>
+            <option value="ARGENTO">Argento</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group className="m-3">
+          <Form.Select value={tipo} onChange={(e) => setTipo(e.target.value)}>
+            <option value="">Tipo</option>
+            <option value="jeans">Jeans</option>
+            <option value="pantaloni">Pantaloni</option>
+            <option value="gonna">Gonna</option>
+            <option value="pantaloncini">Pantaloncini</option>
+            <option value="TOP">Top</option>
+            <option value="maglioncino">Maglioncino</option>
+            <option value="maglia">Maglia</option>
+            <option value="felpa">Felpa</option>
+            <option value="camicia">Camicia</option>
+            <option value="canottiera">Canottiera</option>
+            <option value="BODY">Body</option>
+            <option value="tuta">Tuta</option>
+            <option value="abito">Abito</option>
+            <option value="borsa">Borsa</option>
+            <option value="cintura">Cintura</option>
+            <option value="sneakers">Sneakers</option>
+            <option value="sandali">Sandali</option>
+            <option value="tacchi">Tacchi</option>
+            <option value="anfibi">Anfibi</option>
+            <option value="stivali">Stivali</option>
+            <option value="sciarpa">Sciarpa</option>
+            <option value="cappotto">Cappotto</option>
+            <option value="trench">Trench</option>
+            <option value="giacca">Giacca</option>
+            <option value="giubotto">Giubotto</option>
+          </Form.Select>
         </Form.Group>
 
         <Button type="submit" className="custom-button">
