d89f20d68b9b305b4e08c2b495c331e3a6f8ed21
MichelaVivacqua <vivacquamichela@gmail.com>
2024-05-13 22:29:11 +0200

    no

--- src/components/Valigia.jsx
+++ src/components/Valigia.jsx
@@ -1,15 +1,62 @@
 import { useState, useEffect } from "react";
-import { Form, Button, Dropdown, Container, Row, Col } from "react-bootstrap";
+import { Form, Button, Dropdown, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "../assets/OIG4 (6).jpg";
+import Card from "react-bootstrap/Card";
 
 const Valigia = () => {
   const [outfitCount, setOutfitCount] = useState(1);
   const [selectedSeason, setSelectedSeason] = useState(null);
   const [outfits, setOutfits] = useState([]);
   const [selectedOutfits, setSelectedOutfits] = useState([]);
-
-  const seasons = ["Primavera/Autunno", "Estate", "Inverno"];
+  const seasonsMap = {
+    Estate: [
+      "PANTALONCINI",
+      "TOP",
+      "BODY",
+      "GONNA",
+      "MAGLIA",
+      "CANOTTIERA",
+      "BORSA",
+      "CINTURA",
+      "TACCHI",
+      "ABITO",
+    ],
+    Inverno: [
+      "CAPPOTTO",
+      "TRENCH",
+      "JEANS",
+      "PANTALONI",
+      "MAGLIONCINO",
+      "FELPA",
+      "BORSA",
+      "CINTURA",
+      "SNEAKERS",
+      "TACCHI",
+      "ANFIBI",
+      "STIVALI",
+      "TUTA",
+      "SCIARPA",
+      "CAPPOTTO",
+      "GIUBOTTO",
+      "ABITO",
+    ],
+    PrimaveraAutunno: [
+      "GIUBBOTTO",
+      "JEANS",
+      "PANTALONI",
+      "MAGLIA",
+      "CAMICIA",
+      "BORSA",
+      "CINTURA",
+      "SNEAKERS",
+      "TACCHI",
+      "TRENCH",
+      "GIACCA",
+      "GIUBOTTO",
+      "ABITO",
+    ],
+  };
 
   useEffect(() => {
     const fetchMyOutfits = async () => {
@@ -128,24 +175,37 @@ const Valigia = () => {
                   {selectedSeason ? selectedSeason : "Seleziona stagione"}
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
-                  {seasons.map((season) => (
-                    <Dropdown.Item
-                      key={season}
-                      onClick={() => handleFilterBySeason(season)}
-                    >
-                      {season}
-                    </Dropdown.Item>
-                  ))}
+                  <Dropdown.Item
+                    key="Estate"
+                    onClick={() => handleFilterBySeason("Estate")}
+                  >
+                    <i className="bi bi-brightness-high mx-1"></i>
+                    Estate
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                    key="PrimaveraAutunno"
+                    onClick={() => handleFilterBySeason("PrimaveraAutunno")}
+                  >
+                    <i className="bi bi-cloud-sun mx-1"></i>
+                    Primavera/Autunno
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                    key="Inverno"
+                    onClick={() => handleFilterBySeason("Inverno")}
+                  >
+                    <i className="bi bi-snow2 mx-1"></i>
+                    Inverno
+                  </Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
             </Form.Group>
-            <Button
+            {/* <Button
               variant="secondary"
               onClick={handleChooseOutfits}
               className="m-3"
             >
               Cambia Outfits
-            </Button>
+            </Button> */}
             <Button
               variant="success"
               className="m-3"
@@ -156,6 +216,37 @@ const Valigia = () => {
           </Form>
         </Col>
       </Row>
+      {outfits
+        .filter(
+          (outfit) =>
+            !selectedSeason ||
+            outfit.indumenti.every((indumento) =>
+              seasonsMap[selectedSeason].includes(indumento.tipo)
+            )
+        )
+        .map((outfit) => (
+          <Card key={outfit.id} className="m-1 col-12 col-md-3">
+            <Card.Body>
+              <Card.Title>Outfit {outfit.id}</Card.Title>
+              <Card.Text>
+                {outfit.indumenti &&
+                  outfit.indumenti.map((indumento) => (
+                    <div key={indumento.id}>
+                      <Card.Img
+                        variant="top"
+                        src={indumento.image}
+                        alt={indumento.tipo}
+                        style={{ width: "100px", height: "auto" }}
+                        className="align-self-center"
+                      />
+                      <p>{indumento.tipo}</p>
+                      <p>{indumento.colore}</p>
+                    </div>
+                  ))}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
     </div>
   );
 };
