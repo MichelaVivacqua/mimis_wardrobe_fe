716180c996d0c1be755b3433975c31844dcad781
MichelaVivacqua <vivacquamichela@gmail.com>
2024-05-24 15:03:41 +0200

    test parti statiche

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,25 +1,55 @@
 import { render, screen } from "@testing-library/react";
 import Login from "./components/Login";
 import { BrowserRouter } from "react-router-dom";
+import CreaIndumento from "./components/CreaIndumento";
+import FiltroTipologia from "./components/FiltroTipologia";
 
-test("renders login form with email and password inputs and login button", () => {
+test("Verifico che i campi email,password e login siano presenti", () => {
   render(
     <BrowserRouter>
       <Login />
     </BrowserRouter>
   );
 
-  // Verifica che il campo email sia presente
   const emailInput = screen.getByPlaceholderText(/Inserisci qui la tua email/i);
   expect(emailInput).toBeInTheDocument();
 
-  // Verifica che il campo password sia presente
   const passwordInput = screen.getByPlaceholderText(
     /Inserisci qui la tua password/i
   );
   expect(passwordInput).toBeInTheDocument();
 
-  // Verifica che il pulsante di login sia presente
   const loginButton = screen.getByText(/Accedi/i);
   expect(loginButton).toBeInTheDocument();
+});
+
+test("Verifico che i campi immagine indumento, tipo e colore e il tasto di submit siano presenti", () => {
+  render(
+    <BrowserRouter>
+      <CreaIndumento />
+    </BrowserRouter>
+  );
+
+  const tipo = screen.getByText(/tipo/i);
+  expect(tipo).toBeInTheDocument();
+
+  const colore = screen.getByText(/colore/i);
+  expect(colore).toBeInTheDocument();
+
+  const submit = screen.getByText(/salva/i);
+  expect(submit).toBeInTheDocument();
+});
+
+test("Verifica che il componente Armadio filtro tipologia venga renderizzato correttamente", () => {
+  render(
+    <BrowserRouter>
+      <FiltroTipologia />
+    </BrowserRouter>
+  );
+
+  const logo = screen.getByAltText("logo");
+  expect(logo).toBeInTheDocument();
+
+  const dropdown = screen.getByText(/Seleziona tipo/i);
+  expect(dropdown).toBeInTheDocument();
 });
