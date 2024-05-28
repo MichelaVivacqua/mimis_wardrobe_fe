8a1dc70465961ce6ad7b9bfebad509c6d99088cf
MichelaVivacqua <vivacquamichela@gmail.com>
2024-05-28 18:58:29 +0200

    Update FetchWeatherData.jsx

--- src/components/meteo/FetchWeatherData.jsx
+++ src/components/meteo/FetchWeatherData.jsx
@@ -2,7 +2,7 @@
 
 const FetchWeatherData = async (city) => {
   try {
-    const API_KEY = process.env.REACT_APP_API_KEY;
+    const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
 
     const API_BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=it`;
 
